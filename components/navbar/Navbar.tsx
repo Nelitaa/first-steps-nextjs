3e20241567e1b3316d4cee67dae0c58019b3d6b0
Marianela Munoz Gutierrez <marianelamunozgutierrez@gmail.com>
2024-01-20 20:15:43 -0400

    Replace anchor tag with next/link

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,16 +1,31 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import { HomeIcon } from '@primer/octicons-react';
+
+const navItems = [
+  { text: 'About', path: '/about' },
+  { text: 'Pricing', path: '/pricing' },
+  { text: 'Contact', path: '/contact' },
+];
 
 export const Navbar = () => {
   return (
   <nav className="flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
 
-    <span>Home</span>
+    <Link href={'/'} className='flex items-center'>
+      <HomeIcon className='mr-2' />
+      <span>Home</span>
+    </Link>
 
     <div className='flex flex-1'></div>
 
-    <a className="mr-2" href="/about">About</a>
-    <a className="mr-2" href="/pricing">Pricing</a>
-    <a className="mr-2" href="/contact">Contact</a>
+    {
+      navItems.map((item) => (
+        <Link key={item.path} href={item.path} className='mr-2'>
+          {item.text}
+        </Link>
+      ))
+    }
 
   </nav>
   )
